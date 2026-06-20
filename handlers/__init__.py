310fd5121467745aee92c60fac9d426387601a2b
WeteroScript <weterochina@gmail.com>
2026-06-21 02:42:21 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,13 @@
+# handlers/__init__.py
 
+from .handlers import register_handlers
+from .admin import register_admin_handlers
+from .business import register_business_handlers
+from .user import register_user_handlers
+
+__all__ = [
+    'register_handlers',
+    'register_admin_handlers', 
+    'register_business_handlers',
+    'register_user_handlers'
+]
