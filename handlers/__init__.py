2f4e2e7f44da87763e9a65262e4295f33b684590
WeteroScript <weterochina@gmail.com>
2026-06-21 02:44:58 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,13 +1,12 @@
 # handlers/__init__.py
 
-from .handlers import register_handlers
+# Убираем импорт из .handlers, так как его нет в папке handlers
 from .admin import register_admin_handlers
 from .business import register_business_handlers
 from .user import register_user_handlers
 
 __all__ = [
-    'register_handlers',
-    'register_admin_handlers', 
+    'register_admin_handlers',
     'register_business_handlers',
     'register_user_handlers'
 ]
