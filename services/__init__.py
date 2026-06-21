6ce5a368058947a6cde8d3463a702928cf11048d
WeteroScript <weterochina@gmail.com>
2026-06-21 22:14:14 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,44 @@
+from .currency import currency_rates
+from .tasks import (
+    promo_auto_loop, check_business_loop, 
+    promo_running, promo_task, 
+    business_running, business_check_task
+)
+from .auction import (
+    auction_running, 
+    auction_update_task, 
+    auction_timers,
+    get_auction_lots_for_display, 
+    place_bid,
+    update_auction_lots, 
+    auction_update_loop,
+    set_admin_auction_lots, 
+    refresh_auction_for_all,
+    get_stars_display, 
+    get_stars_by_rarity,
+    user_auction_page,
+    frozen_bids
+)
 
+__all__ = [
+    'currency_rates',
+    'promo_auto_loop',
+    'check_business_loop',
+    'promo_running',
+    'promo_task',
+    'business_running',
+    'business_check_task',
+    'auction_running',
+    'auction_update_task',
+    'auction_timers',
+    'get_auction_lots_for_display',
+    'place_bid',
+    'update_auction_lots',
+    'auction_update_loop',
+    'set_admin_auction_lots',
+    'refresh_auction_for_all',
+    'get_stars_display',
+    'get_stars_by_rarity',
+    'user_auction_page',
+    'frozen_bids'
+]
